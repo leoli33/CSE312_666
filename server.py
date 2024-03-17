7709aa7d691ffa7a82c147c821ca82d18891db0e
Ycaihui1536 <103393946+Ycaihui1536@users.noreply.github.com>
2024-03-16 22:51:04 -0400

    updated javascript file for message and upadted the message html along with the server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, session
 
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
 
 mongo_client = MongoClient("mongo")
@@ -28,13 +28,22 @@ def post_html():
     return render_template('post.html')
 
 @app.route('/message', methods=['GET', 'POST', 'PUT'])
-def message_html():
-   return render_template('message.html')
-
+def message():
+    username = session.get('username', 'Guest')
+    return render_template('message.html', username=username)
+    
 @socketio.on("chat_message")
 def user_input(message):
-   chat_collection.insert_one({"username": "Unauthorized_guest", "message": message})
-   print(message)
+    sender = message["sender"]
+    messages = message["message"]
+
+    messages = messages.replace("&", "&amp")
+    messages = messages.replace("<", "&lt")
+    messages = messages.replace(">", "&gt")
+
+    chat_collection.insert_one({"username": sender, "message": messages})
+    emit("load_chat", {"username": sender, "message": messages},broadcast=True)
+    print(message)
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=8080)
--- static/functions.js
+++ static/functions.js
@@ -1,9 +1,33 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', function(){
+    const socket = io();
+    const chatMessages = document.getElementById("chat-messages");
+    const clearButton = document.getElementById("clear-button");
+    const chatForm = document.getElementById('chat-form');
 
-document.getElementById("send-button").addEventListener("click", function(){
-    let message = document.getElementById("chat-input").value.trim();
-    if (message !== "") {
-        socket.emit("chat_message", message);
-        document.getElementById("chat-input").value = ""; 
-    }
-});
+    chatForm.addEventListener('submit',function(event) {
+        event.preventDefault();
+        let message = document.getElementById("chat-input").value.trim();
+        if (message !== "") {
+            socket.emit("chat_message", {sender: username, message: message});
+            document.getElementById("chat-input").value = ""; 
+        }
+    })
+
+    socket.on("load_chat", function(data) {
+        const sender = data.username;
+        const message = data.message;
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('message-container');
+        const messageElement = document.createElement('default');
+        messageElement.textContent = sender + ": " + message;
+        messageDiv.appendChild(messageElement);
+        chatMessages.appendChild(messageDiv);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    });
+
+        clearButton.addEventListener('click', function() {
+            while (chatMessages.firstChild) {
+                chatMessages.removeChild(chatMessages.firstChild);
+            }
+        })
+})
