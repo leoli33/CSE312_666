559ee61be5b7b72fdad46f1688bbf9a12a3a26a8
marysunc <112438166+marysunc@users.noreply.github.com>
2024-04-21 03:43:32 -0400

    fixed not working websocket message.js

--- static/js/message.js
+++ static/js/message.js
@@ -1,4 +1,4 @@
-const socket = io({transports:['websocket']});
+const socket = io('wss://cupid-666.me', { path: '/socket.io', transports: ['websocket'] });
 const chatMessages = document.getElementById("chat-messages");
 const clearButton = document.getElementById("clear-button");
 const chatForm = document.getElementById('chat-form');
